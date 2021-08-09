887d767b48cb7f6e6fde570a36cd47cb507e2644
NDboy <endre.karsai@gmail.com>
2021-08-10 00:30:40 +0200

    vééééégre!!!!!!!!!!!!!!!!!! most már flyway-jel is

--- src/main/resources/db/migration/V1__createPlayer.sql
+++ src/main/resources/db/migration/V1__createPlayer.sql
@@ -2,8 +2,8 @@ create table players
 (
     id            bigint auto_increment,
     name          varchar(255) not null,
-    birth_date    varchar(255) not null,
-    position_type varchar(255) not null,
+    birth_date    date not null,
+    position      varchar(255) not null,
     team_id       bigint,
     primary key (id)
 )
